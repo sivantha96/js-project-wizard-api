6d8b9bf097197cfc9a6388a7faa85c3c7ed3a07a
Sivantha <sivantha.paranavithana@gmail.com>
2021-08-24 18:46:39 +0530

    New file system added

--- lib/modules/files/controllers/files.controller.ts
+++ lib/modules/files/controllers/files.controller.ts
@@ -19,7 +19,6 @@ import {
     expressSwagger,
     expressTsConfigJson,
 } from '../constants/express.constants';
-import { generateExpressFile } from '../helpers/express-file-generator.helper';
 
 export class FilesController {
     // get express script
@@ -27,103 +26,125 @@ export class FilesController {
         const name = req.query.name;
         const description = req.query.description;
         const author = req.query.author;
-
-        let stringifiedPackageJson,
-            stringifiedTsConfigJson,
-            stringifiedGitIgnore,
-            stringifiedPrettierrc,
-            stringifiedEslintrc,
-            stringifiedEnv,
-            stringifiedServerConfig,
-            stringifiedMongoConfig,
-            stringifiedSwagger,
-            stringifiedCommonConstants,
-            stringifiedErrorConstants,
-            stringifiedResponseMessagesConstants,
-            stringifiedErrorHandler,
-            stringifiedResponseService,
-            stringifiedCommonRoutes,
-            stringifiedRoutes,
-            stringifiedServer;
-
         try {
-            stringifiedPackageJson = await axios.get(expressPackageJson);
-            stringifiedPackageJson = JSON.stringify(stringifiedPackageJson.data);
+            const packageJson = {
+                name: 'package.json',
+                path: '.',
+                content: (await axios.get(expressPackageJson)).data,
+            };
 
-            stringifiedTsConfigJson = await axios.get(expressTsConfigJson);
-            stringifiedTsConfigJson = JSON.stringify(stringifiedTsConfigJson.data);
+            packageJson.content.name = name;
+            packageJson.content.description = description;
+            packageJson.content.author = author;
 
-            stringifiedGitIgnore = await axios.get(expressGitIgnore);
-            stringifiedGitIgnore = JSON.stringify(stringifiedGitIgnore.data);
+            const jsonFiles = [
+                packageJson,
+                {
+                    name: 'tsconfig.json',
+                    path: '.',
+                    content: (await axios.get(expressTsConfigJson)).data,
+                },
+                {
+                    name: '.prettierrc',
+                    path: '.',
+                    content: (await axios.get(expressPrettierrc)).data,
+                },
+                {
+                    name: 'swagger.json',
+                    path: './lib/config',
+                    content: (await axios.get(expressSwagger)).data,
+                },
+            ];
+            const otherFiles = [
+                {
+                    name: '.gitignore',
+                    path: '.',
+                    content: (await axios.get(expressGitIgnore)).data,
+                },
+                {
+                    name: '.eslintrc.js',
+                    path: '.',
+                    content: (await axios.get(expressEslintrc)).data,
+                },
+                {
+                    name: '.env',
+                    path: '.',
+                    content: (await axios.get(expressEnv)).data,
+                },
+                {
+                    name: '.env.local',
+                    path: '.',
+                    content: (await axios.get(expressEnv)).data,
+                },
+                {
+                    name: 'routes.ts',
+                    path: './lib',
+                    content: (await axios.get(expressRoutes)).data,
+                },
+                {
+                    name: 'server.ts',
+                    path: './lib',
+                    content: (await axios.get(expressServer)).data,
+                },
+                {
+                    name: 'server.config.ts',
+                    path: './lib/config',
+                    content: (await axios.get(expressServerConfig)).data,
+                },
+                {
+                    name: 'mongo.config.ts',
+                    path: './lib/config',
+                    content: (await axios.get(expressMongoConfig)).data,
+                },
+                {
+                    name: 'routes.ts',
+                    path: './lib/modules/common',
+                    content: (await axios.get(expressCommonRoutes)).data,
+                },
+                {
+                    name: 'common.constant.ts',
+                    path: './lib/modules/common/constants',
+                    content: (await axios.get(expressCommonConstants)).data,
+                },
+                {
+                    name: 'error.constant.ts',
+                    path: './lib/modules/common/constants',
+                    content: (await axios.get(expressErrorConstants)).data,
+                },
+                {
+                    name: 'response-messages.constant.ts',
+                    path: './lib/modules/common/constants',
+                    content: (await axios.get(expressResponseMessagesConstants)).data,
+                },
+                {
+                    name: 'global-error-handler.middleware.ts',
+                    path: './lib/modules/common/middleware',
+                    content: (await axios.get(expressErrorHandler)).data,
+                },
+                {
+                    name: 'response.service.ts',
+                    path: './lib/modules/common/services',
+                    content: (await axios.get(expressResponseService)).data,
+                },
+            ];
 
-            stringifiedPrettierrc = await axios.get(expressPrettierrc);
-            stringifiedPrettierrc = JSON.stringify(stringifiedPrettierrc.data);
+            let mainFile = `const fs = require("fs"); const { series } = require("async"); const { execSync } = require("child_process"); series([ () => {execSync("git init -q"); console.log("Setting up the project boilerplate");`;
 
-            stringifiedEslintrc = await axios.get(expressEslintrc);
-            stringifiedEslintrc = JSON.stringify(stringifiedEslintrc.data);
+            jsonFiles.forEach((file) => {
+                mainFile += ` if(!fs.existsSync("${file.path}")){fs.mkdirSync("${file.path}", { recursive: true });} fs.writeFileSync('${
+                    file.path
+                }/${file.name}', '${JSON.stringify(file.content)}');`;
+            });
 
-            stringifiedEnv = await axios.get(expressEnv);
-            stringifiedEnv = JSON.stringify(stringifiedEnv.data);
+            otherFiles.forEach((file) => {
+                mainFile += ` if(!fs.existsSync("${file.path}")){fs.mkdirSync("${file.path}", { recursive: true });} fs.writeFileSync('${
+                    file.path
+                }/${file.name}', ${JSON.stringify(file.content)});`;
+            });
 
-            stringifiedServerConfig = await axios.get(expressServerConfig);
-            stringifiedServerConfig = JSON.stringify(stringifiedServerConfig.data);
-
-            stringifiedMongoConfig = await axios.get(expressMongoConfig);
-            stringifiedMongoConfig = JSON.stringify(stringifiedMongoConfig.data);
-
-            stringifiedSwagger = await axios.get(expressSwagger);
-            stringifiedSwagger = JSON.stringify(stringifiedSwagger.data);
-
-            stringifiedCommonConstants = await axios.get(expressCommonConstants);
-            stringifiedCommonConstants = JSON.stringify(stringifiedCommonConstants.data);
-
-            stringifiedErrorConstants = await axios.get(expressErrorConstants);
-            stringifiedErrorConstants = JSON.stringify(stringifiedErrorConstants.data);
-
-            stringifiedResponseMessagesConstants = await axios.get(expressResponseMessagesConstants);
-            stringifiedResponseMessagesConstants = JSON.stringify(stringifiedResponseMessagesConstants.data);
-
-            stringifiedErrorHandler = await axios.get(expressErrorHandler);
-            stringifiedErrorHandler = JSON.stringify(stringifiedErrorHandler.data);
-
-            stringifiedResponseService = await axios.get(expressResponseService);
-            stringifiedResponseService = JSON.stringify(stringifiedResponseService.data);
-
-            stringifiedCommonRoutes = await axios.get(expressCommonRoutes);
-            stringifiedCommonRoutes = JSON.stringify(stringifiedCommonRoutes.data);
-
-            stringifiedRoutes = await axios.get(expressRoutes);
-            stringifiedRoutes = JSON.stringify(stringifiedRoutes.data);
-
-            stringifiedServer = await axios.get(expressServer);
-            stringifiedServer = JSON.stringify(stringifiedServer.data);
-
-            const mainFile = generateExpressFile(
-                name,
-                description,
-                author,
-                stringifiedPackageJson,
-                stringifiedTsConfigJson,
-                stringifiedGitIgnore,
-                stringifiedPrettierrc,
-                stringifiedEslintrc,
-                stringifiedEnv,
-                stringifiedServerConfig,
-                stringifiedMongoConfig,
-                stringifiedSwagger,
-                stringifiedCommonConstants,
-                stringifiedErrorConstants,
-                stringifiedResponseMessagesConstants,
-                stringifiedErrorHandler,
-                stringifiedResponseService,
-                stringifiedCommonRoutes,
-                stringifiedRoutes,
-                stringifiedServer,
-            );
+            mainFile += `execSync("npm install"); execSync("git add ."); execSync("git commit -m 'Initial commit'"); console.log("Project Initialized Successfully.")},]);`;
 
             res.send(mainFile);
-        } catch (error) {
-            console.log(error);
-        }
+        } catch (error) {}
     }
 }
