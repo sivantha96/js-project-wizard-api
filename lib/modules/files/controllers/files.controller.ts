58f63d79ebb4222d97404414090d055401c9671f
Sivantha <sivantha.paranavithana@gmail.com>
2022-03-07 02:33:20 +0530

    Update: rn template versions

--- lib/modules/files/controllers/files.controller.ts
+++ lib/modules/files/controllers/files.controller.ts
@@ -156,20 +156,27 @@ export class FilesController {
     // generate react native script
     public async generateReactNativeScript(req: Request, res: Response) {
         try {
-            const { name, description, author, hasReactNavigation, hasRedux, hasTheming, hasI18n } = req.body;
+            const { name, description, author, hasReactNavigation, hasRedux, hasVectorIcons, hasTheming, hasI18n } = req.body;
 
             let templateVersion;
             switch (true) {
-                case hasReactNavigation && hasRedux && !hasTheming && !hasI18n:
+                case hasReactNavigation && hasRedux && !hasTheming && !hasI18n && hasVectorIcons: // redux + navigation + vector icons
                     templateVersion = '0.0.1';
                     break;
 
-                case hasReactNavigation && !hasRedux && !hasTheming && !hasI18n:
+                case hasReactNavigation && !hasRedux && !hasTheming && !hasI18n && !hasVectorIcons: // navigation
                     templateVersion = '0.0.3';
                     break;
 
-                case hasRedux && !hasReactNavigation && !hasTheming && !hasI18n:
+                case hasRedux && !hasReactNavigation && !hasTheming && !hasI18n && !hasVectorIcons: // redux
                     templateVersion = '0.0.4';
+
+                case hasVectorIcons && !hasReactNavigation && !hasTheming && !hasI18n && !hasRedux: // vector icons
+                    templateVersion = '0.0.5';
+                    break;
+
+                case hasVectorIcons && !hasReactNavigation && !hasTheming && !hasI18n && hasRedux: // redux + vector icons
+                    templateVersion = '0.0.6';
                     break;
 
                 default:
